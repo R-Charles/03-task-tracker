5b514b09ac8f82f6783ffb300762643bf9d9245a
Raphael Charles <106124732+R-Charles@users.noreply.github.com>
2026-02-07 17:09:05 -0800

    modify addTask

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import AddTask from "./components/AddTask"
 
 function App() {
   return (
     <>
-      <h1 className="text-2xl font-bold py-4 pl-6">03 - The Task 
-      Tracker</h1>
+      <h1 className="text-2xl font-bold py-4 pl-6">03 - The Task Tracker</h1>
       <p className="text-xl pl-6">Hi there!</p>
-      <p className="text-xl pl-6">Click</p> 
-        +New 
-      to add a new task</p>
+      <div>
+        <p className="text-xl pl-6">Click</p> 
+          <AddTask />
+        <p className="text-xl my-2">to add a new task</p>
+      </div>
     </>
   );
 }
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 const AddTask = () => {
+    const [addModal, setAddModal] = useState(false);
+
     return(
         <>
-        <button>+New</button>
+            <button 
+                className="bg-blue-500 text-white uppercase text-sm font-semibold py-1 mx-1.5 pl-2 pr-2.5 rounded hover:opacity-70"
+                type="button"
+                onClick={() => setAddModal(true)}
+            > 
+                + New
+            </button>
+            {addModal ? (
+                <>
+                    <div className="flex items-center justify-center overflow-x-hidden overflow-y-auto fixed insert-0 z-100">
+                        <h3>Add New Task</h3>
+                    </div>
+                </>
+            ) : null}
         </>
     )
 }
